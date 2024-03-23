40c9aba78e321ae7baf4750a34f62b971ae761df
alex <monteleone.1883922@studenti.uniroma1.it>
2024-03-23 16:33:05 +0100

    traduzione tags + bug fixes

--- managing_data_task_23.py
+++ managing_data_task_23.py
@@ -8,36 +8,38 @@
 import json
 import pprint as pp
 import random as rnd
+import sys
 
 SENTENCE_START = "_____"
 
 DECODE_TAGS = {
-    "ADJ": "Adjective",  # aggettivo
-    "ADP": "Adposition",  # preposizione semplice
-    "ADP A": "Adposition+Article",  # preposizione articolata
-    "ADV": "Adverb",  # avverbio
-    "AUX": "Auxiliary Verb",  # verbo ausiliare
-    "CONJ": "Coordinating Conjunction",  # congiunzione coordinativa
-    "DET": "Determiner",  # determinante
-    "INTJ": "Interjection",  # interiezione
-    "NOUN": "Noun",  # nome
-    "NUM": "Numeral",  # numero
-    "PART": "Particle",  # particella
-    "PRON": "Pronoun",  # pronome
-    "PROPN": "Proper Noun",  # nome proprio
-    "PUNCT": "punctuation",  # punteggiatura
-    "SCONJ": "Subordinating Conjunction",  # congiunzione subordinativa
-    "SYM": "Symbol",  # simbolo
-    "EMO": "Emoticon",  # emoticon
-    "URL": "Web Address",  # indirizzo web
-    "EMAIL": "Email Address",  # indirizzo email
-    "HASHTAG": "Hashtag",  # hashtag
-    "MENTION": "Mention",  # menzione
-    "VERB": "Verb",  # verbo
-    "VERB CLIT": "Verb + Clitic pronoun cluster",  # verbo + pronome clitico
-    "X": "Other"  # altro
+    "ADJ": "aggettivo",
+    "ADP": "preposizione semplice",
+    "ADP_A": "preposizione articolata",
+    "ADV": "avverbio",
+    "AUX": "verbo ausiliare",
+    "CONJ": "congiunzione coordinativa",
+    "DET": "determinante",
+    "INTJ": "interiezione",
+    "NOUN": "sostantivo",
+    "NUM": "numero",
+    "PART": "particella",
+    "PRON": "pronome",
+    "PROPN": "nome proprio",
+    "PUNCT": "punteggiatura",
+    "SCONJ": "congiunzione subordinativa",
+    "SYM": "simbolo",
+    "EMO": "emoticon",
+    "URL": "indirizzo web",
+    "EMAIL": "indirizzo email",
+    "HASHTAG": "hashtag",
+    "MENTION": "menzione",
+    "VERB": "verbo",
+    "VERB_CLIT": "verbo + pronome clitico",
+    "X": "altro"
 }
 MAX_COMMON_TAGS_TO_CONSIDER = 7
+
 
 def get_list_sentences(file_path):
     with open(file_path, 'r', encoding='utf8') as file:
@@ -48,7 +50,7 @@ def get_list_sentences(file_path):
                 if not first_line:
                     sentences.append(sentence)
                 first_line = False
-                sentence = [(line.strip(SENTENCE_START), "")]  # sentence id
+                sentence = [(line.strip().strip(SENTENCE_START), "")]  # sentence id
             elif line.strip() != "":
                 sentence.append(tuple(line.split()))
         sentences.append(sentence)
@@ -70,13 +72,15 @@ def get_info_sentences(sentences):
 def extract_most_common_tags(sentences_info):
     list_tags = list(sentences_info["tags"].items())
     list_tags.sort(key=lambda x: x[1], reverse=True)
-    return [tag[0] for tag in list_tags]
+    return [DECODE_TAGS[tag[0]] for tag in list_tags]
 
 
 def generate_distractors(word, tag, tag_position, most_common_tags_for_words, most_common_tags):
     choiches = []
-    tags_for_word = most_common_tags_for_words[word]
+    tag = DECODE_TAGS[tag]
+    tags_for_word = most_common_tags_for_words[word].copy()
     tags_for_word.remove(tag)
+    tags_for_word = list(tags_for_word)
     tmp = [i for i in range(len(tags_for_word))]
     rnd.shuffle(tmp)
     for i in range(min(len(tmp), 3)):
@@ -94,9 +98,6 @@ def generate_distractors(word, tag, tag_position, most_common_tags_for_words, mo
             choiches.append(choice)
     choiches.insert(tag_position, tag)
     return choiches
-
-
-
 
 
 def find_repeated_words(sentences):
@@ -124,11 +125,13 @@ def build_common_tags_for_word(sentences):
     common_tags = {}
     for sentence in sentences:
         for word in sentence[1:]:
-            common_tags[word[0]] = common_tags.get(word[0], []) + [word[1]]
+            tags_word = common_tags.get(word[0], set())
+            tags_word.add(DECODE_TAGS[word[1]])
+            common_tags[word[0]] = tags_word
     return common_tags
 
 
-def create_json(sentences):
+def create_json(sentences, file_name):
     json_sentences = []
     most_common_tags_for_words = build_common_tags_for_word(sentences)
     most_common_tags = extract_most_common_tags(get_info_sentences(sentences))
@@ -142,18 +145,28 @@ def create_json(sentences):
                 "sentence": sentence_text,
                 "target_word": word[0],
                 "word_idx": i,
-                "choices": generate_distractors(word[0], word[1], label, most_common_tags_for_words, most_common_tags),
+                "choices": generate_distractors(word[0], word[1], label, most_common_tags_for_words,
+                                                most_common_tags.copy()),
                 "label": label
             }
             json_sentences.append(sentence_dict)
-
+    with open(file_name, 'w') as jsonl_file:
+        for item in json_sentences:
+            json.dump(item, jsonl_file)  # Scrivi l'oggetto JSON
+            jsonl_file.write('\n')
 
 
 if __name__ == '__main__':
-    sentences = get_list_sentences("data/postwita/goldDEVset-2016_09_05_anon_rev.txt")
+    if len(sys.argv) != 3:
+        print("Usage: python managing_data_task_23.py <input_file> <output_file>")
+        sys.exit(1)
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    sentences = get_list_sentences(input_file)
+    # create_json(sentences, output_file)
     info = get_info_sentences(sentences)
     print(extract_most_common_tags(info))
-    pp.pprint(info)
+    # pp.pprint(info)
     # dangerous_sentences, dangerous_sentences_idx, dangerous_words = find_repeated_words(sentences)
     # with open("dangerous_sentences.txt", 'w', encoding='utf8') as file:
     #     for sentence in dangerous_sentences:
