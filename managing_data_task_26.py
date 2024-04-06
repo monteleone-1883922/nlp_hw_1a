b8bdabccbcd69badb32c966a27aae8b031ed102b
alex <monteleone.1883922@studenti.uniroma1.it>
2024-04-06 16:24:51 +0200

    fixed problems with the usage of embeddings

--- managing_data_task_26.py
+++ managing_data_task_26.py
@@ -2,20 +2,42 @@ import editdistance
 from heapq import heappop, heappush, heapify
 import time, sys, json
 import random as rnd
+import fasttext.util
+
+
 
 VOCABULARY_PATH = "data/SemEval2018-Task9/1B.italian.vocabulary.txt"
 K = 5
+NEIGHBORS_POOL = 15
 
 
 def get_vocabulary():
     return set([line.strip().lower() for line in open(VOCABULARY_PATH, 'r')])
 
 
-def get_distance(word1, word2):
-    return editdistance.eval(word1, word2)
+def get_possible_distractors(targets: set[str], vocabulary: set[str], embeddings = None):
+    return get_possible_distractors_by_edit_dist(targets, vocabulary) if embeddings is None else get_neighbors(targets, embeddings, vocabulary)
 
 
-def get_possible_distractors(targets: set[str], vocabulary: set[str]):
+def get_neighbors(targets, embeddings, vocabulary):
+    distractors = set()
+    for target in targets:
+        distractors.union(get_vocabulary_neighbors(target, vocabulary, embeddings))
+    return distractors
+
+def get_vocabulary_neighbors(target, vocabulary, embeddings):
+    neighbors = embeddings.get_nearest_neighbors(target, k=NEIGHBORS_POOL)
+    similar_words = set()
+    for neighbor in neighbors:
+        if neighbor in vocabulary:
+            similar_words.add(neighbor)
+        if len(similar_words) == K:
+            break
+    return similar_words
+
+
+
+def get_possible_distractors_by_edit_dist(targets: set[str], vocabulary: set[str]):
     distractors = set()
     for target in targets:
         heap = []
@@ -23,10 +45,10 @@ def get_possible_distractors(targets: set[str], vocabulary: set[str]):
         for word in vocabulary:
             if word not in distractors and word not in targets:
                 if len(heap) < K:
-                    heappush(heap, (-1 * get_distance(target, word), word))
+                    heappush(heap, (-1 * editdistance.eval(target, word), word))
                 elif editdistance.eval(target, word) < -1 * heap[0][0]:
                     heappop(heap)
-                    heappush(heap, (-1 * get_distance(target, word), word))
+                    heappush(heap, (-1 * editdistance.eval(target, word), word))
         for word in heap:
             distractors.add(word[1])
     return distractors
@@ -69,17 +91,26 @@ def create_json(data, output_file, vocabulary):
 
 
 def test():
+    fasttext.util.download_model('it', if_exists='ignore')  # Italian
+    embeddings = fasttext.load_model('cc.it.300.bin')
+    print(embeddings.get_nearest_neighbors("cane_gatto", k=1))
+
+def main():
+    if len(sys.argv) < 4:
+        print("Usage: python managing_data_task_26.py <input_file1> <input_file2> <output_file>")
+        sys.exit(1)
+    hyponym_file = sys.argv[1]
+    hypernym_file = sys.argv[2]
+    output_file = sys.argv[3]
+    data = get_data(hyponym_file, hypernym_file)
     vocabulary = get_vocabulary()
-    targets = ["sesto", "grado", "numero ordinale", "frazione", "carica"]
-    tmp = time.time()
-    distractors = get_possible_distractors(set(targets), vocabulary)
-    print("the function has been executed in ", time.time() - tmp, " seconds")
-    print(distractors)
+    create_json(data, output_file, vocabulary)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("Usage: python managing_data_task_23.py <input_file1> <input_file2> <output_file>")
+    if len(sys.argv) < 4:
+        print("Usage: python managing_data_task_26.py <input_file1> <input_file2> <output_file>")
         sys.exit(1)
     input_file = sys.argv[1]
     output_file = sys.argv[2]
+    test()
